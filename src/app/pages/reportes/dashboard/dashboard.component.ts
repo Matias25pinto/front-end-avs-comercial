99853a460e8626270e8f3f7148a66dd80ca91dd6
Matias Pinto <matias25pinto@gmail.com>
2021-12-09 00:18:58 -0300

    FIX(dashboard):Bug de fechas

--- src/app/pages/reportes/dashboard/dashboard.component.ts
+++ src/app/pages/reportes/dashboard/dashboard.component.ts
@@ -9,33 +9,28 @@ import { ReportesService } from '../../../services/reportes.service';
 export class DashboardComponent implements OnInit {
   public formulario: FormGroup;
   public isLoading: boolean = false;
+  //Total General
+  public totalVentas: number = 0;
+  public totalCompras: number = 0;
+
+  //Mejor vendedor
+  public mejorVendedor: any = {
+    id: 0,
+    nombre_vendedor: 'Vendedor',
+    ventas_totales: 0,
+  };
+
   //PIE-1
   //OBJETO DE PRODUCTOS ESTO DEBE VENIR DEL BACK-END
-  public productos: any[] = [
-    { nombre: 'producto1', totalVenta: 400000, totalGanancia: 150000 },
-    { nombre: 'producto2', totalVenta: 200000, totalGanancia: 100000 },
-    { nombre: 'producto3', totalVenta: 400000, totalGanancia: 200000 },
-    { nombre: 'producto4', totalVenta: 300000, totalGanancia: 150000 },
-    { nombre: 'producto5', totalVenta: 500000, totalGanancia: 200000 },
-  ];
-  public vendedores: any[] = [
-    { nombre: 'vendedor 1', totalVenta: 300000 },
-    { nombre: 'vendedor 2', totalVenta: 400000 },
-    { nombre: 'vendedor 3', totalVenta: 500000 },
-    { nombre: 'vendedor 4', totalVenta: 200000 },
-  ];
-  //Total General
-  public totalVentas:number = 1000000;
-  public totalCompras:number = 400000;
-  //CALCULOS EN EL FRONT-END CON LA DATA productos
+  public productos: any[] = [];
+  public totalProductosPie: number = 0;
+  public subTotalProductosPie: number[] = [];
+  public titulo1 = 'Los productos más vendidos en porcentaje';
   public nombreProductos: string[] = [];
-  public totalVentasPie: number = 0;
-  public subTotalVentaPie: number[] = [];
-  public titulo1 = 'Los 5 productos más vendidos';
 
   //BAR-1
+  public vendedores: any[] = [];
   public titulo2 = 'Total de Ventas de los vendedores';
-  public variables2 = [];
   public serie1 = 'Total Ventas Gs.';
   public valorVariablesSerie1 = [];
   public nombreVendedores: string[] = [];
@@ -43,41 +38,93 @@ export class DashboardComponent implements OnInit {
   //General
   public cantidadVentasDias = 0;
 
-  constructor(private reportes: ReportesService, private fb: FormBuilder) {
+  constructor(
+    private reportesService: ReportesService,
+    private fb: FormBuilder
+  ) {
     this.formulario = this.fb.group({ fechaInicio: [''], fechaFin: [''] });
-    //Calculo para pie-1
-    this.productos.map((producto) => {
-      this.nombreProductos.push(producto.nombre);
-      this.subTotalVentaPie.push(producto.totalVenta);
-      this.totalVentasPie = this.totalVentasPie + producto.totalVenta;
-    });
-
-    //calculo para bar-1
-    this.vendedores.map((vendedor) => {
-      this.nombreVendedores.push(vendedor.nombre);
-      this.valorVariablesSerie1.push(vendedor.totalVenta);
-    });
   }
 
   ngOnInit(): void {
     this.cargarFecha();
   }
+  cargarDatos(fechaInicio: string, fechaFin: string) {
+    const body = { fecha_inicio: fechaInicio, fecha_fin: fechaFin };
+    this.reportesService.totalCompras(body).subscribe((resp) => {
+      this.totalCompras = resp['total_compras'];
+    });
+    this.reportesService.totalVentas(body).subscribe((resp) => {
+      this.totalVentas = resp['total_ventas'];
+    });
+
+    this.reportesService.cantidadArticulosVendidos(body).subscribe((resp) => {
+      this.productos = resp;
+      //Calculo para pie-1
+      resp.map((producto) => {
+        this.nombreProductos.push(producto.nombre_articulo);
+        this.subTotalProductosPie.push(producto.cantidad);
+        this.totalProductosPie = this.totalProductosPie + producto.cantidad;
+      });
+    });
+
+    this.reportesService.mejorVendedor(body).subscribe((resp) => {
+      this.mejorVendedor = resp;
+    });
+    this.reportesService.getVendedores(body).subscribe((resp) => {
+      this.vendedores = resp;
+      //calculo para bar-1
+      this.vendedores.map((vendedor) => {
+        this.nombreVendedores.push(vendedor.nombre_apellido);
+        this.valorVariablesSerie1.push(vendedor.total);
+      });
+    });
+  }
+  limpiarFormulario() {
+    this.totalCompras = 0;
+    this.totalVentas =0;
+    this.productos = [];
+    this.nombreProductos = [];
+    this.subTotalProductosPie = [];
+    this.totalProductosPie = 0;
+    this.mejorVendedor = {
+      id: 0,
+      nombre_vendedor: 'Vendedor',
+      ventas_totales: 0,
+    };
+
+    this.vendedores = [];
+    this.valorVariablesSerie1 = [];
+    this.nombreVendedores = [];
+  }
+
   cargarFecha() {
     const fecha = new Date();
     const month = fecha.getMonth() + 1;
     const year = fecha.getFullYear();
-    const fechaInicio = new Date(`${year}/${month}/${1}`);
+    const fechaInicio = new Date(`${year}/${month}/1`);
     const fechaFin = new Date(
       `${year}/${month < 12 ? month - 1 : month}/${month < 12 ? 0 : 31}`
     );
+
     this.formulario.reset({ fechaInicio: fechaInicio, fechaFin: fechaFin });
+    this.cargarDatos(
+      `${year}-${month}-${1}`,
+      `${year}-${month < 12 ? month - 1 : month}-${month < 12 ? 0 : 31}`
+    );
   }
   enviarFormulario() {
     if (this.formulario.valid) {
+      this.limpiarFormulario();
       const fechaInicio = this.formulario.get('fechaInicio')?.value;
       const fechaFin = this.formulario.get('fechaFin')?.value;
-
-      console.log('fechaInicio: ', fechaInicio, ' fechaFin: ', fechaFin);
+      this.cargarDatos(
+        `${fechaInicio.getFullYear()}-${
+          fechaInicio.getMonth() + 1
+        }-${fechaInicio.getDate()}`,
+        `${fechaFin.getFullYear()}-${
+          fechaFin.getMonth() + 1
+        }-${fechaFin.getDate()}`
+      );
     }
   }
 }
--- src/app/services/reportes.service.ts
+++ src/app/services/reportes.service.ts
@@ -10,24 +10,82 @@ export class ReportesService {
 
   constructor(private http: HttpClient) {}
 
-  cantidadVentasDia() {
+  totalCompras(body: any) {
     const access_token = `Token ${localStorage.getItem('access_token')}`;
     let headers = new HttpHeaders({
       authorization: access_token,
     }).set('Content-Type', 'application/json');
 
-    return this.http.get(`${this.url}/reportes/reporte-ventas-dia`, {
+    return this.http.post(`${this.url}/reportes/reporte-total-compras/`, body, {
       headers,
     });
   }
+  totalVentas(body: any) {
+    const access_token = `Token ${localStorage.getItem('access_token')}`;
+    let headers = new HttpHeaders({
+      authorization: access_token,
+    }).set('Content-Type', 'application/json');
+
+    return this.http.post(`${this.url}/reportes/reporte-total-ventas/`, body, {
+      headers,
+    });
+  }
+
+  cantidadArticulosVendidos(body: any) {
+    const access_token = `Token ${localStorage.getItem('access_token')}`;
+    let headers = new HttpHeaders({
+      authorization: access_token,
+    }).set('Content-Type', 'application/json');
+
+    return this.http.post<Array<any>>(
+      `${this.url}/reportes/reporte-cantidad-articulos-vendidos/`,
+      body,
+      {
+        headers,
+      }
+    );
+  }
+  mejorVendedor(body: any) {
+    const access_token = `Token ${localStorage.getItem('access_token')}`;
+    let headers = new HttpHeaders({
+      authorization: access_token,
+    }).set('Content-Type', 'application/json');
+
+    return this.http.post<Array<any>>(
+      `${this.url}/reportes/reporte-mejor-vendedor/`,
+      body,
+      {
+        headers,
+      }
+    );
+  }
+
+  getVendedores(body: any) {
+    const access_token = `Token ${localStorage.getItem('access_token')}`;
+    let headers = new HttpHeaders({
+      authorization: access_token,
+    }).set('Content-Type', 'application/json');
+
+    return this.http.post<Array<any>>(
+      `${this.url}/reportes/reporte-vendedores/`,
+      body,
+      {
+        headers,
+      }
+    );
+  }
+
   getArticulosStockMinimo() {
     const access_token = `Token ${localStorage.getItem('access_token')}`;
     let headers = new HttpHeaders({
       authorization: access_token,
     }).set('Content-Type', 'application/json');
 
-    return this.http.get<Array<any>>(`${this.url}/reportes/reporte-articulos-stock/`, {
-      headers,
-    });
+    return this.http.get<Array<any>>(
+      `${this.url}/reportes/reporte-articulos-stock/`,
+      {
+        headers,
+      }
+    );
   }
 }
